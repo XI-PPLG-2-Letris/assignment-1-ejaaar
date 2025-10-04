a8dcc566cecc6971f0160d29b43a6565187c980c
Arbi.PNG <156532958+ejaaar@users.noreply.github.com>
2025-10-05 00:33:38 +0700

    Add files via upload

---
+++ tugas1/script.js
@@ -0,0 +1,19 @@
+console.log('tugas 1');
+
+const a = 10
+const b = 5
+
+const tambah = () => a + b;
+console.log(tambah()); // hasil 10 + 5 = 15
+
+const kurang = () => a - b;
+console.log(kurang()); // hasil 10 - 5 = 5
+
+const kali = () => a * b;
+console.log(kali()); // hasil 10 * 5 = 50
+
+const bagi = () => a / b;
+console.log(bagi()); // hasil 10 / 5 = 2
+
+const modulus = () => a % b;
+console.log(modulus()); // hasil 10 % 5 = 0 
